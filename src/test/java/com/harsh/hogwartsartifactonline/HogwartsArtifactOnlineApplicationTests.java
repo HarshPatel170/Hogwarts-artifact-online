2256a1f3f0b27c1796fcca7b61e25d610510bb1a
Purvish17 <123947080+Purvish17@users.noreply.github.com>
2024-03-09 13:09:09 +0530

    This is first commit.

---
+++ src/test/java/com/harsh/hogwartsartifactonline/HogwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.hogwartsartifactonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
